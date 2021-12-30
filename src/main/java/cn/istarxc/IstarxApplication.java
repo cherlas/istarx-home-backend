328f8ead0d7992de128dd62d9c42963d1a290181
cherlas <istarxc@gmail.com>
2021-12-30 20:35:31 +0800

    solve the sql connect exception of druid

--- src/main/java/cn/istarxc/IstarxApplication.java
+++ src/main/java/cn/istarxc/IstarxApplication.java
@@ -1,5 +1,6 @@
 package cn.istarxc;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -8,7 +9,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @author istarxc.cn
  * @date 2021/12/29
  */
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
 public class IstarxApplication {
     public static void main(String[] args) {
         SpringApplication.run(IstarxApplication.class, args);
