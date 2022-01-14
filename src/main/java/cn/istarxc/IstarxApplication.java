98f9250f08205c43305b343ebfdc46afd7b3b63a
cherlas <istarxc@gmail.com>
2022-01-15 00:42:36 +0800

    feat(support for mvnw): turn project from web to webflux

--- src/main/java/cn/istarxc/IstarxApplication.java
+++ src/main/java/cn/istarxc/IstarxApplication.java
@@ -1,15 +1,16 @@
 package cn.istarxc;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author istarxc.cn
  * @date 2021/12/29
  */
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@PropertySource(value={"classpath:database.properties"})
 public class IstarxApplication {
     public static void main(String[] args) {
         SpringApplication.run(IstarxApplication.class, args);
