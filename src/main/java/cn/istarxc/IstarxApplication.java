b4304f8221c71d1ed5b1531a9c2a4326078d1147
cherlas <istarxc@gmail.com>
2022-01-24 10:07:45 +0800

    build: change build tools to gradle

--- src/main/java/cn/istarxc/IstarxApplication.java
+++ src/main/java/cn/istarxc/IstarxApplication.java
@@ -2,15 +2,12 @@ package cn.istarxc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * @author istarxc.cn
  * @date 2021/12/29
  */
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@PropertySource(value={"classpath:database.properties"})
+@SpringBootApplication
 public class IstarxApplication {
     public static void main(String[] args) {
         SpringApplication.run(IstarxApplication.class, args);
---
+++ src/main/java/cn/istarxc/common/constant/CommonSymbols.java
@@ -0,0 +1,15 @@
+package cn.istarxc.common.constant;
+
+/**
+ * @author istarx
+ * @date 2022-1-14
+ */
+public final class CommonSymbols {
+    public static final String COLON = ":";
+    public static final String SLASH = "/";
+    public static final String DOUBLE_SLASH = "//";
+    public static final String COMMA = ",";
+    public static final String SINGLE_QUOTATION = "'";
+    public static final String DOUBLE_QUOTATION = "\"";
+    public static final String AT_SIGN = "@";
+}
