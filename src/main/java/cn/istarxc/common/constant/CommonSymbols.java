4ebf249a1922cc30ffecb2eea0ef872f565f3ce5
cherlas <istarxc@gmail.com>
2022-01-24 14:13:16 +0800

    chore: change common symol class to enum to fix code analysis warnings

--- src/main/java/cn/istarxc/common/constant/CommonSymbols.java
+++ src/main/java/cn/istarxc/common/constant/CommonSymbols.java
@@ -1,15 +1,27 @@
 package cn.istarxc.common.constant;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * @author istarx
  * @date 2022-1-14
  */
-public final class CommonSymbols {
-    public static final String COLON = ":";
-    public static final String SLASH = "/";
-    public static final String DOUBLE_SLASH = "//";
-    public static final String COMMA = ",";
-    public static final String SINGLE_QUOTATION = "'";
-    public static final String DOUBLE_QUOTATION = "\"";
-    public static final String AT_SIGN = "@";
+@Getter
+@AllArgsConstructor
+public enum CommonSymbols {
+    /**
+     * common symbols in chinese and english
+     */
+    COLON(":", "："),
+    SLASH("/", "/"),
+    DOUBLE_SLASH("//", "//"),
+    COMMA(",", "，"),
+    SINGLE_QUOTATION("'", "'"),
+    LEFT_DOUBLE_QUOTATION("\"", "“"),
+    RIGHT_DOUBLE_QUOTATION("\"", "”"),
+    AT_SIGN("@", "@");
+
+    private final String englishSymbol;
+    private final String chineseSymbol;
 }
